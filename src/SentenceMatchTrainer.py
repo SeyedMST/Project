31835c1f9f7999eae64d9887dff272e69f21bd25
Mohsen <mohsensajjaditabar@gmail.com>
2018-04-25 13:21:05 +0430

    train

--- src/SentenceMatchTrainer.py
+++ src/SentenceMatchTrainer.py
@@ -709,7 +709,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--word_vec_path', type=str, default='../data/glove/glove.6B.50d.txt', help='Path the to pre-trained word vector model.')
     #parser.add_argument('--word_vec_path', type=str, default='../data/glove/glove.840B.300d.txt', help='Path the to pre-trained word vector model.')
-    parser.add_argument('--is_server',default=False, help='loop: ranom initalizaion of parameters -> run ?')
+    parser.add_argument('--is_server',default=True, help='loop: ranom initalizaion of parameters -> run ?')
     parser.add_argument('--max_epochs', type=int, default=10, help='Maximum epochs for training.')
     parser.add_argument('--attention_type', default='dot_product', help='[bilinear, linear, linear_p_bias, dot_product]', action='store_true')
 
