414fa99eac920af0c65281137e97d6e28927c4e0
Mohsen <mohsensajjaditabar@gmail.com>
2018-05-29 19:26:55 +0430

    ver3

--- src/SentenceMatchTrainer.py
+++ src/SentenceMatchTrainer.py
@@ -784,7 +784,7 @@ if __name__ == '__main__':
     #parser.add_argument('--word_vec_path', type=str, default='../data/glove/glove.840B.300d.txt', help='Path the to pre-trained word vector model.')
     parser.add_argument('--is_server',default=False, help='do we have cuda visible devices?')
     parser.add_argument('--is_random_init',default=False, help='loop: ranom initalizaion of parameters -> run ?')
-    parser.add_argument('--max_epochs', type=int, default=1, help='Maximum epochs for training.')
+    parser.add_argument('--max_epochs', type=int, default=10, help='Maximum epochs for training.')
     parser.add_argument('--attention_type', default='dot_product', help='[bilinear, linear, linear_p_bias, dot_product]', action='store_true')
 
     bs =80
