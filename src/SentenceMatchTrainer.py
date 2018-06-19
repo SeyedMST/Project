f17cb86ac442e11d9ea62c844c10a38c01125639
Mohsen <mohsensajjaditabar@gmail.com>
2018-06-20 00:14:41 +0430

    ver3

--- src/SentenceMatchTrainer.py
+++ src/SentenceMatchTrainer.py
@@ -422,7 +422,8 @@ def Get_Next_box_size (index):
     #list = [15, 30, 50, 70, 100, 150, 200, 300, 10, 20, 40, 90, 110, 120] #topsample1- (batch=4)
     #list = [30, 100] #topsample2- (batch = 10)
     #list = [1.5, 2, 3, 4,   5.0,0.1, 100.0, 1.0,1.25] #toptreshold1-
-    list = [100, 100, 100]
+    #list = [100, 100, 100] #samplelist1-
+    list = [100] #divcount
     if  (index > FLAGS.end_batch):
         return False
 
@@ -435,11 +436,13 @@ def Get_Next_box_size (index):
     FLAGS.max_epochs = 8
 
     if index == 0:
-        FLAGS.pos_avg = True
-    if index == 1:
         FLAGS.pos_avg = False
-    else:
-        FLAGS.new_list_wise = False
+    # if index == 0:
+    #     FLAGS.pos_avg = True
+    # if index == 1:
+    #     FLAGS.pos_avg = False
+    # else:
+    #     FLAGS.new_list_wise = False
     FLAGS.word_vec_path = "../data/glove/glove.6B.50d.txt"
     FLAGS.sampling_type = 'attentive'
     # if list [index] < 50:
